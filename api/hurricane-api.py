c7648e41cf24a29c6d124b53802cf197a0b0a265
Andrew Bailey <andrew@awbnyc.com>
2023-04-16 22:06:44 -0400

    Geocoding and risk scores.

--- api/hurricane-api.py
+++ api/hurricane-api.py
@@ -2,13 +2,19 @@ import pandas as pd
 import pickle
 import imblearn
 import random
+import geocoder
+import math
 from typing import Union
 from fastapi import FastAPI
 from sklearn.pipeline import Pipeline
 from joblib import dump, load
 
-estimator = load("best_rf_tuned.joblib")
+## Load lookups and model
 
+estimator = load("best_rf_tuned.joblib")
+lookup_risk_df = pd.read_csv('lookup_risk.csv')
+
+## Serve application
 app = FastAPI()
 
 """ 
@@ -43,6 +49,8 @@ def heartbeat():
     return "Online"
 
 def input_to_df(address, rent, quarter, occupancy, gust, wind, hurricanecat):
+    risk_scores = get_risk_scores(address)
+    
     input_data = {'USA_WIND': [wind], 
         'USA_GUST': [gust], 
         'storm_category': [hurricanecat], 
@@ -52,9 +60,9 @@ def input_to_df(address, rent, quarter, occupancy, gust, wind, hurricanecat):
         'prop_rent_growth': [0.007035],
         'sma_rent_growth': [0.008267], 
         'distance_from_coast': [75.76417], 
-        'RISK_SCORE': [33.495147], 
-        'CFLD_RISKS': [1.988109],
-        'HRCN_RISKS': [18.73492],
+        'RISK_SCORE': [risk_scores['risk_score']], 
+        'CFLD_RISKS': [risk_scores['cfld_risks']],
+        'HRCN_RISKS': [risk_scores['hrcn_risks']],
         'quarter_num': [quarter]}
     data_df = pd.DataFrame.from_dict(input_data, orient='columns')
 
@@ -64,4 +72,21 @@ def get_rent_growth(address):
     x = x
 
 def get_risk_scores(address):
-    y = y
+    risk_scores = {}
+    zipcode = get_loc_info(address)
+    risk_rec = lookup_risk_df[lookup_risk_df['ZIP'] == zipcode]
+    print(risk_rec)
+    risk_scores['risk_score'] = risk_rec['RISK_SCORE'].iloc[0] if not math.isnan(risk_rec['RISK_SCORE'].iloc[0]) else 0.0
+    risk_scores['cfld_risks'] = risk_rec['CFLD_RISKS'].iloc[0] if not math.isnan(risk_rec['CFLD_RISKS'].iloc[0]) else 0.0
+    risk_scores['hrcn_risks'] = risk_rec['HRCN_RISKS'].iloc[0] if not math.isnan(risk_rec['HRCN_RISKS'].iloc[0]) else 0.0
+    return risk_scores
+
+def get_loc_info(address, type="zip"):
+    loc_val = None
+    g = geocoder.osm(address, maxrows=1)
+    if g:
+        if type == "coord":
+            loc_val = g.latlng
+        if type == "zip":
+            loc_val = int(g.postal)
+    return loc_val
